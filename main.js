ba038e2c530ec2ce19e59c93e6517d3e350a8a46
jimena-11 <138959659+jimena-11@users.noreply.github.com>
2023-11-03 10:51:10 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var nombre="lucas"
+var apellido="solis"
+var nombre_completo=nombre.concat(apellido)
+console.log(nombre_completo)
+
+var texto="yo soy harry popotes"
+var correcion=texto.replace("popotes","potter")
+console.log(correcion)
+
+var numero_1=5
+var numero_2=8
+var suma=numero_1+numero_2
+console.log(suma)
+
+var resta=numero_1-numero_2
+console.log(resta)
+
+var cubo=Math.pow(5,3)
+console.log(cubo)
+
